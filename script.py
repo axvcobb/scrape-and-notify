5c63c9022f085d38fb086c291a6d3b318a801b18
Alex Cobb <axvcobb@gmail.com>
2020-03-29 15:25:28 -0400

    refactored Twilio stuff into its own file/class

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from twilio.rest import Client
+from Twilio import Twilio
 from Mailchimp import Mailchimp
 import requests
 import config
@@ -13,21 +13,13 @@ def scrape(url, text):
 
 
 def notify():
-    account_sid = config.twilio_sid
-    auth_token = config.twilio_auth
-    client = Client(account_sid, auth_token)
-
-    client.messages \
-        .create(
-            body=config.twilio_body,
-            from_=config.twilio_from,
-            to=config.twilio_to
-        )
+    Twilio(config.twilio_sid, config.twilio_auth).\
+        send_text(config.twilio_body, config.twilio_from, config.twilio_to)
+    Mailchimp(config.mailchimp_key).\
+        trigger_email(config.mailchimp_url, config.mailchimp_email)
 
 
 # If scrape doesn't find the text, it returns an empty array.
 # If it finds something, it triggers the notify() function
 if scrape(config.scrape_url, config.scrape_text):
     notify()
-    mc = Mailchimp(config.mailchimp_key)
-    mc.trigger_email(config.mailchimp_url, config.mailchimp_email)
